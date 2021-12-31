c36c754217f3fc7900831e010223339d7c150d8e
unknown <personal@akbaraditama.com>
2021-12-31 09:40:23 +0700

    Add tailwind and index.html

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const fs = require("fs");
+const path = require("path");
 
-const httpServer = createServer();
+const httpServer = createServer(function (req, res) {
+  if (req.url !== "/") {
+    var stream = fs.createReadStream(path.join("public", req.url));
+    stream.on("error", function () {
+      res.writeHead(404);
+      res.end();
+    });
+    stream.pipe(res);
+    return;
+  }
+
+  const readStream = fs.createReadStream("./index.html");
+  res.writeHead(200, { "Content-type": "text/html" });
+  readStream.pipe(res);
+});
 const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
