c5d03bd897069255a0e17de668a9275426dd13d6
unknown <personal@akbaraditama.com>
2021-12-31 09:56:24 +0700

    Basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#87AAAA",
+      },
+    },
   },
   plugins: [],
 };
