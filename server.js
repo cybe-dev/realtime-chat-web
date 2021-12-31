fc650dd4194634d18218413e35596802bdc1b9d3
unknown <personal@akbaraditama.com>
2021-12-31 09:18:27 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {});
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000);
